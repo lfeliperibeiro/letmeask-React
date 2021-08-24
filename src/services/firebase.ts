596ab3c23f1cea766d3aa565c920cf7f01b91ef4
Felipe Ribeiro <piporibeiro@gmail.com>
2021-08-24 19:59:00 -0300

    refactor: remove unnecessary export

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -14,6 +14,6 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
-export const auth = firebase.auth;
+const databaseURL = firebase.database;
 
-export const databaseURL = firebase.database;
+export { firebase, databaseURL };
