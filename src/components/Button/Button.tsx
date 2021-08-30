34fbab145ad708e6939c6970e7ded9259f8b92f2
Felipe Ribeiro <piporibeiro@gmail.com>
2021-08-29 21:30:29 -0300

    feat: add props of the button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,10 +4,24 @@ import cx from 'classnames';
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   isOutlined?: boolean;
+  isDeleted?: boolean;
+  isCanceled?: boolean;
 };
 
-export function Button({ isOutlined = false, ...props }: ButtonProps) {
+export function Button({
+  isOutlined = false,
+  isCanceled = false,
+  isDeleted = false,
+  ...props
+}: ButtonProps) {
   return (
-    <button className={cx('button', { outlined: isOutlined })} {...props} />
+    <button
+      className={cx('button', {
+        outlined: isOutlined,
+        cancel: isCanceled,
+        delete: isDeleted,
+      })}
+      {...props}
+    />
   );
 }
